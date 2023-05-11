124a69774b2bc0376cfe7d4140ccde8839744b8e
3equals3 <105347293+3equals3@users.noreply.github.com>
2023-05-11 08:38:37 +0300

    Update main.js

--- example/build/main.js
+++ example/build/main.js
@@ -388,11 +388,12 @@ System.register(["imgui-js", "./imgui_impl.js", "./imgui_demo.js", "./imgui_memo
                     let aspect_ratio = new ImGui.Vec2(1/((ImGui.GetWindowSize().x) / 850), 1/((ImGui.GetWindowSize().y-99) / 866));
                     if(menustate == 0){
                         const uv_min = new ImGui.Vec2(0.0, 0.0);
-                        const uv_max = new ImGui.Vec2(aspect_ratio.y, aspect_ratio.y);
+                        const uv_max = new ImGui.Vec2(aspect_ratio.x, aspect_ratio.y);
                         const tint_col = new ImGui.Vec4(1.0, 1.0, 1.0, 1.0);
                         const border_col = new ImGui.Vec4(1.0, 1.0, 1.0, 0.0); // 50% opaque white
                         ImGui.SetCursorPos(new ImGui.Vec2(((ImGui.GetWindowSize().x - (850/aspect_ratio.y))*0.5+1), 100));
                         ImGui.Image(image_texture, new ImGui.Vec2(850, 866), uv_min, uv_max, tint_col, border_col);
+                        //DrawIMG(image_texture, ImGui.GetWindowSize().x);
 
                         ImGui.GetIO().FontGlobalScale = 1;
                         ImGui.PushFont(ImGui.GetIO().Fonts.Fonts[6]);
